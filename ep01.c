526f06afec96b78b210973a6bbf4a69065529129
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-08-17 21:13:08 -0300

    Alocação de cabeçalho

--- ep01.c
+++ ep01.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
+#define MEM_SIZE 16384
+
 typedef struct block{
 	struct block *b, *n;
 	void *val;
 }block;
-void* mem[16384]; //16Kb
+typedef struct part{
+	void * pos;
+	size_t size;
+}part;
+
+void* mem[MEM_SIZE]; //16Kb
+const size_t cabecalho_tam = MEM_SIZE/4;
 
 /* PROTÓTIPOS */
 void * aloca(size_t nbytes);
 void   libera(void *p);
 
+void * aloca(size_t nbytes){
+	
+}
 
 int main(){
 	printf("Olá mundo!");
