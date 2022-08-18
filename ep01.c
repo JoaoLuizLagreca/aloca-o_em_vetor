85be269af607e4644ee2c5d906c85289475acbc9
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-08-18 19:43:58 -0300

    Fechando a chave que esqueci de fechar

--- ep01.c
+++ ep01.c
@@ -16,7 +16,7 @@ const size_t cabecalho_tam = MEM_SIZE/4;
 void initMemory(){
 	/* Garanta que o cabeçalho não tenha lixo no início do programa */
 	int i;
-	for(i=0; i<cabecalho_tam; i++){
+	for(i=0; i<cabecalho_tam; i++)
 		mem[i]=0x00;
 }
 
