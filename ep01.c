3646d24c6f9f4083ed22deb00fcdff7319884037
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-08-17 22:15:00 -0300

    Inicialização de cabeçalho

--- ep01.c
+++ ep01.c
@@ -13,6 +13,12 @@ typedef struct part{
 
 void* mem[MEM_SIZE]; //16Kb
 const size_t cabecalho_tam = MEM_SIZE/4;
+void initMemory(){
+	/* Garanta que o cabeçalho não tenha lixo no início do programa */
+	int i;
+	for(i=0; i<cabecalho_tam; i++){
+		mem[i]=0x00;
+}
 
 /* PROTÓTIPOS */
 void * aloca(size_t nbytes);
@@ -23,5 +29,6 @@ void * aloca(size_t nbytes){
 }
 
 int main(){
+	initMemory();
 	printf("Olá mundo!");
 }
